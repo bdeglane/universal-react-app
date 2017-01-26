938b0418aa73be93be5b831cb070a3f89413c054
B D <bdeglane@users.noreply.github.com>
2017-01-26 13:11:14 +0100

    update

--- src/client/core/Store.js
+++ src/client/core/Store.js
@@ -22,6 +22,9 @@ const crashReporter = store => next => action => {
     return next(action)
   } catch (err) {
     console.error('Caught an exception!', err);
+
+    // todo ajax call with axios to log id db error
+
     //Raven.captureException(err, {
     //    extra: {
     //        action,
@@ -84,11 +87,6 @@ export default class Store {
     });
   }
 
-  /**
-   *
-   *
-   * @returns {Store<S>|*}
-   */
   getStore() {
     return this.store;
   }
