dbd438072bac6288cb58442b91e0131c44afab04
B D <bdeglane@users.noreply.github.com>
2017-01-26 17:16:17 +0100

    create home page component

---
+++ src/client/page/home/Home.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Home page</h1>
+      </div>
+    )
+  }
+}
