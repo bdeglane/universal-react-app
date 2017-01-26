0157778ebe750ac09af461c84e71e297cac1d05f
B D <bdeglane@users.noreply.github.com>
2017-01-26 12:17:24 +0100

    add helper to react and redux dev connect to redux dev

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,32 +1,101 @@
 import React, {Component} from 'react';
-import {createStore, combineReducers} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import {browserHistory} from 'react-router'
-import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
+import {syncHistoryWithStore} from 'react-router-redux'
+import thunk from 'redux-thunk';
+
+import reducers from './reducer/index';
 import Routes from './route/Routes.jsx';
 
-// Add the reducer to your store on the `routing` key
-const store = createStore(
-  combineReducers({
-    // ...reducers,
-    reducers: {},
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.group(action.type);
+      console.info('dispatching', action);
+      // store dispatch
+      let result = next(action);
+      console.log('next state', store.getState());
+      console.groupEnd(action.type);
+      return result
+    };
+  };
+};
 
-    routing: routerReducer
-  })
-);
+const crashReporter = store => next => action => {
+  try {
+    return next(action)
+  } catch (err) {
+    console.error('Caught an exception!', err);
+    //Raven.captureException(err, {
+    //    extra: {
+    //        action,
+    //        state: store.getState()
+    //    }
+    //});
+    throw err
+  }
+};
 
-// Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store);
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return 'undefined';
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return 'undefined';
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (error) {
+    // ignore errors
+  }
+};
+
+const initialState = () => {
+  let state = loadState();
+  if (state === 'undefined') {
+    return {}
+  } else {
+    return state;
+  }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default class App extends Component {
   constructor() {
     super();
+    // Add the reducer to your store on the `routing` key
+    this.store = createStore(
+      reducers,
+      initialState(),
+      composeEnhancers(
+        applyMiddleware(
+          thunk,
+          logger,
+          // crashReporter
+        )
+      )
+    );
+    // Create an enhanced history that syncs navigation events with the store
+    this.history = syncHistoryWithStore(browserHistory, this.store);
+    // Store state in local storage
+    this.store.subscribe(() => {
+      saveState(this.store.getState());
+    });
   }
 
   render() {
     return (
-      <Provider store={store}>
-        <Routes history={history}/>
+      <Provider store={this.store}>
+        <Routes history={this.history}/>
       </Provider>
     )
   }
