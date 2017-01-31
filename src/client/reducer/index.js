3e6de99d218fbc12cf4592dc89f422bf2a8d9dd7
B D <bdeglane@users.noreply.github.com>
2017-01-31 14:13:40 +0100

    register menu reducer

--- src/client/reducer/index.js
+++ src/client/reducer/index.js
@@ -1,12 +1,15 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
+import {menu} from './menu';
+
 /**
  * Combine all the reducers in a constant and export it.
  * This constant will be given to the redux store
  */
 const reducers = combineReducers({
-  routing: routerReducer
+  routing: routerReducer,
+  menu
 });
 
 export default reducers;
