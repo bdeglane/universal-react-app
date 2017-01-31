d33cfafbb929d07e9b6c44ad0e92fdd81ed4bde5
B D <bdeglane@users.noreply.github.com>
2017-01-31 14:14:26 +0100

    connect SidebarNav to redux

---
+++ src/client/component/sidebarNav/index.js
@@ -0,0 +1,14 @@
+import {connect} from 'react-redux';
+import SidebarNav from './SidebarNav.jsx';
+
+const mapStateToProps = (state) => {
+  return {
+    menu: state.menu.open
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps,null)(SidebarNav);
