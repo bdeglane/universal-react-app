e773158e928ecbe64cd4f01dcac8e5608687284f
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-03-28 14:53:22 +0200

    removed first version

--- src/client/middleware/promiseMiddleware.js
+++ src/client/middleware/promiseMiddleware.js
@@ -1,53 +1,3 @@
-// Promise middleware for redux as explain in redux doc
-// see http://redux.js.org/docs/advanced/Middleware.html
-export const promiseMiddleware = (objMethods) => {
-  return (next) => {
-    return (action) => {
-
-      const {promise, types, ...rest} = action;
-
-      // 假如傳來的 action 內沒有 promise 屬性，代表不需 async 處理，直接略過
-      if (!promise) {
-        // console.log( 'promiseMiddleware > 沒 promise > 不處理，且接丟給後手' );
-        return next(action);
-      }
-
-      // 這裏聰明的將外界傳入的變數，透過 destructuring 轉為常數
-      // 因此 middleware 可適用於各種不同情境
-      const [REQUEST, SUCCESS, ERROR] = types;
-
-      // console.log( '建立 Const: ', REQUEST, SUCCESS, ERROR );
-
-      // 進行第一次的廣播，讓畫面立即更新，也就是 optimistic update
-      next({...rest, type: REQUEST});
-
-      // 然後偵聽 WebAPI promise 操作結束，發出第二次廣播
-      // 這次 type 改為 SUCCESS，因此 store 內知道要依 tid 更新 uid
-      return promise.then(
-        (result) => next({...rest, result, type: SUCCESS}),
-        (error) => next({...rest, error, type: ERROR})
-      );
-    };
-  };
-};
-
-/**
- * Lets you dispatch promises in addition to actions.
- * If the promise is resolved, its result will be dispatched as an action.
- * The promise is returned from `dispatch` so the caller may handle rejection.
- */
-export const vanillaPromise = store => next => action => {
-  // if action have a promise key
-  if (!action.promise) {
-    return next(action)
-  }
-  // wait for the promise to resolve
-  // before calling next action
-  return Promise
-    .resolve(action.promise())
-    .then(store.dispatch);
-};
-
 /**
  * Lets you dispatch special actions with a { promise } field.
  *
@@ -83,7 +33,7 @@ export const readyStatePromise = (store) => {
       next(makeAction(false));
       // push action in queue by returning
       // an action with pending promise
-      return action.promise.then(
+      return action.promise().then(
         // then resolved, next
         result => next(makeAction(true, {result})),
         error => next(makeAction(true, {error}))
